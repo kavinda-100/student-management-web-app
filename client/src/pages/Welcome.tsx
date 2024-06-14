da0b8e78e6f68ca6372d735fbd47f83b211bd9af
kavinda <kavindarathnayake100@gmail.com>
2024-06-14 20:10:50 +0530

    add welcome text gradient

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -6,11 +6,14 @@ const Welcome = () => {
     <section className="grid h-screen place-items-center">
       <div className="w-full">
         <div className="items-center justify-center">
-          <h1 className="text-2xl font-bold text-center text-gray-700 md:text-4xl text-pretty dark:text-gray-400 font-palanquin">
-            <span className="text-3xl xl:text-6xl">Welcome!</span>{" "}
+          <h1 className="text-2xl font-bold text-center text-transparent md:text-4xl text-pretty font-palanquin bg-clip-text bg-gradient-to-r from-gray-500 to-purple-400 dark:from-blue-300 dark:to-purple-400">
+            <span className="text-3xl font-bold text-transparent xl:text-6xl bg-clip-text bg-gradient-to-r from-blue-600 to-purple-500">
+              Welcome!
+            </span>{" "}
+            <br />
             Manage your classes, grades, and resources all in one place. <br />
           </h1>
-          <h3 className="text-xl font-bold text-center text-gray-700 md:text-2xl text-pretty dark:text-gray-400 font-palanquin">
+          <h3 className="text-xl font-bold text-center text-transparent md:text-2xl text-pretty font-palanquin bg-clip-text bg-gradient-to-r from-gray-400 to-purple-400 dark:from-blue-200 dark:to-purple-200">
             Streamline your academic life! Access your schedule, assignments,
             and announcements with ease.
           </h3>
